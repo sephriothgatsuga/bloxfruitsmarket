24318503f5f8d782c08bc2352809f611af42ef94
sephriothgatsuga <letsugaskael@yahoo.com>
2020-06-11 15:51:56 +0800

    multiple change beta

--- commands/owner/change.js
+++ commands/owner/change.js
@@ -10,26 +10,45 @@ module.exports = {
     },
     run: async (bot, message, args) => {
 
-        if(message.member.roles.cache.has('720243516734832711')){
-          
-          var fruit = args[0];
-          var option = args[1];
-          if(fruit.startsWith('Human') || fruit.startsWith('Bird')){
-            option = args[2];
-            fruit = args.splice(0, 2).join(' ');
-          }
-          
-          
-          if(option === 'true'){
-            option = true;
-          } else if(option === 'false'){
-            option = false;
-          } else {
-            return message.channel.send('Option Invalid!')
-          }
+        const db = (await datab).db('heroku_vf3mq7pv').collection('fruits');
+        let option = args[args.length - 1];
+        let fruit;
 
-          let update = (await datab).db('heroku_vf3mq7pv').collection('fruits').findOneAndUpdate({ name: fruit }, { $set: { instock: option } });
-            update.then(() => message.channel.send('Fruit stock changed!'))
+        if(message.member.roles.cache.has('720243516734832711')){
+          if(args[0] === 'multiple'){
+            fruit = args.slice(1, -1);
+            // option true/false
+            if(option === 'true'){
+              option = true;
+            } else if(option === 'false'){
+              option = false;
+            } else {
+              return message.channel.send('Option Invalid!');
+            }
+            
+            fruit.forEach(f => {
+              let update = db.updateMany({ name: f }, { $set: { instock: option } });
+                update.then(() => message.channel.send('Fruits stock changed!'));
+            });
+            
+          } else{
+            //working with args
+            fruit = args[0];
+            if(fruit.startsWith('Human') || fruit.startsWith('Bird')){
+              fruit = args.splice(0, 2).join(' ');
+            }
+            // option true/false
+            if(option === 'true'){
+              option = true;
+            } else if(option === 'false'){
+              option = false;
+            } else {
+              return message.channel.send('Option Invalid!')
+            }
+            //db update
+            let update = db.findOneAndUpdate({ name: fruit }, { $set: { instock: option } });
+              update.then(() => message.channel.send('Fruit stock changed!'));
+            }
         } else{
           return message.channel.send('You\'re not authorized to do this!');
         }
