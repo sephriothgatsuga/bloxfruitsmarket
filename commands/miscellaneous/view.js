396382e49be36ad682749abf50bf7e2c2fadc3c6
sephriothgatsuga <letsugaskael@yahoo.com>
2020-06-11 20:14:06 +0800

    added restock time

--- commands/miscellaneous/view.js
+++ commands/miscellaneous/view.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js');
 const { getColorFromURL } = require('color-thief-node');
 const datab = require('../../utils/database/database');
 const date = require('date-and-time');
+const cd = require('countdown');
 
 module.exports = { 
     config: {
@@ -16,8 +17,30 @@ module.exports = {
         let color = await getColorFromURL(bot.user.displayAvatarURL({ format: 'png' }))
         let datee = date.format(message.createdAt, 'hh:mm A [GMT]Z', true);
 
+        //countdown
+        var start = new Date();
+        var end = new Date();
+        //conditions
+        if(start.getHours() >= 0 && start.getHours() < 4){
+            end.setHours(4, 0, 0);
+        } else if(start.getHours() >= 4 && start.getHours() < 8){
+            end.setHours(8, 0, 0);
+        } else if(start.getHours() >= 8 && start.getHours() < 12){
+            end.setHours(12, 0, 0);
+        } else if(start.getHours() >= 12 && start.getHours() < 16){
+            end.setHours(16, 0, 0);
+        } else if(start.getHours() >= 16 && start.getHours() < 20){
+            end.setHours(20, 0, 0);
+        } else if(start.getHours() >= 20 && start.getHours() < 24){
+            end.setDate(end.getDate() + 1);
+            end.setHours(0, 0, 0);
+        }
+
+        let cdown = cd(start,  end,cd.HOURS|cd.MINUTES);
+
         var stocky = new MessageEmbed()
             .setTitle(`Current Stock as of ${datee}`)
+            .setDescription(`Verified Fruit Stock\n\nNext Stock is in: ${cdown.hours} Hour(s), ${cdown.minutes} Minute(s)`)
             .setColor(color)
             .setThumbnail(bot.user.displayAvatarURL())
             .setFooter('Stocker is life', bot.user.displayAvatarURL())
