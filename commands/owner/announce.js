90bdbf15d23239e0fbc3178a1a3a33599b5eca64
sephriothgatsuga <letsugaskael@yahoo.com>
2020-06-11 19:50:20 +0800

    announce betaer

--- commands/owner/announce.js
+++ commands/owner/announce.js
@@ -2,6 +2,7 @@ const datab = require('../../utils/database/database');
 const { MessageEmbed } = require('discord.js');
 const { getColorFromURL } = require('color-thief-node');
 const date = require('date-and-time');
+const cd = require('countdown');
 
 module.exports = { 
     config: {
@@ -19,9 +20,30 @@ module.exports = {
 
             let color = await getColorFromURL(bot.user.displayAvatarURL({ format: 'png' }))
             let datee = date.format(message.createdAt, 'hh:mm A [GMT]Z', true);
+            //countdown
+            var start = new Date();
+            var end = new Date();
+            //conditions
+            if(start.getHours() >= 0 && start.getHours() < 4){
+                end.setHours(4, 0, 0);
+            } else if(start.getHours() >= 4 && start.getHours() < 8){
+                end.setHours(8, 0, 0);
+            } else if(start.getHours() >= 8 && start.getHours() < 12){
+                end.setHours(12, 0, 0);
+            } else if(start.getHours() >= 12 && start.getHours() < 16){
+                end.setHours(16, 0, 0);
+            } else if(start.getHours() >= 16 && start.getHours() < 20){
+                end.setHours(20, 0, 0);
+            } else if(start.getHours() >= 20 && start.getHours() < 24){
+                end.setDate(end.getDate() + 1);
+                end.setHours(0, 0, 0);
+            }
+
+            let cdown = cd(start,  end,cd.HOURS|cd.MINUTES);
 
             var stocky = new MessageEmbed()
                 .setTitle(`Current Stock as of ${datee}`)
+                .setDescription(`Verified Fruit Stock\n\nNext Stock is in: ${cdown.hours} Hour(s), ${cdown.minutes} Minute(s)`)
                 .setColor(color)
                 .setThumbnail(bot.user.displayAvatarURL())
                 .setFooter('Stocker is life', bot.user.displayAvatarURL())
@@ -32,8 +54,11 @@ module.exports = {
                 stocky.addField('Ability Levels', `${fruit.abilevels}`, true)
             });
 
-            let channel = bot.guilds.cache.map(g => g.channels.cache.find(ch => ch.name === 'fruit-stock'));
-                channel.forEach(ch => ch.send('hi'));
+            message.guild.channels.cache.find(ch => ch.name === 'test').send('@everyone', stocky).then(m => {
+                setInterval(() => {
+                    m.edit(stocky)
+                }, 30000)
+            });
         } else{
             return message.channel.send('You\'re not authorized to do this!');
         }
