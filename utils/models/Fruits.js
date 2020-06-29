9f17e5fd5df368c9055a19de3127f446423538e8
Nino Pagaran <wordm5862@gmail.com>
2020-06-29 23:23:15 +0800

    Changed to mongoose package

---
+++ utils/models/Fruits.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const FruitSchema = new mongoose.Schema({
+    name: String,
+    bprice: String,
+    rprice: String,
+    type: String,
+    abilevels: String,
+    instock: Boolean
+});
+
+const Fruit = module.exports = mongoose.model('Fruit', FruitSchema);
