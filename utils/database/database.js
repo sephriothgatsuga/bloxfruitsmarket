4cfcd08554cab91535a3c6e028549bc8dcb09007
Nino Pagaran <wordm5862@gmail.com>
2020-07-05 10:05:48 +0800

    stringed uri

--- utils/database/database.js
+++ utils/database/database.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 const uri = process.env.DATABASE;
-module.exports = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+module.exports = mongoose.connect(`${uri}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
