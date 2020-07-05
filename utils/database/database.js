4af0ad26a7053b7069932d8ac6823c35d933e3d3
Nino Pagaran <wordm5862@gmail.com>
2020-07-05 10:09:31 +0800

    git

--- utils/database/database.js
+++ utils/database/database.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-const uri = process.env.DATABASE;
-module.exports = mongoose.connect(`${uri}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+const uri = process.env.MONGODB_URI;
+module.exports = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
