66641a6d6a06b4e9db66a2e61aca45a6f4a796eb
Nino Pagaran <letsugaskael@yahoo.com>
2020-07-09 21:00:57 +0800

    Updated security

--- commands/owner/uptime.js
+++ commands/owner/uptime.js
@@ -18,7 +18,7 @@ module.exports = {
         const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
         return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds`
     }
-    if(!message.author.id === '173977882765426688') return
+    if(message.author.id !== '173977882765426688') return console.log('not owner');
     message.channel.send(`I have been online for: ${duration(bot.uptime)},\nand since ${date.format(bot.readyAt, 'hh:mm A [GMT]Z')}`)
 
     }
