c786eea3ee153492b3ced08469c4b30f7f0afdfe
Nino Pagaran <letsugaskael@yahoo.com>
2020-07-25 12:25:16 +0800

    Updated fail-safe system when having custom prefix

--- commands/owner/change.js
+++ commands/owner/change.js
@@ -1,4 +1,5 @@
 const Fruits = require('../../utils/models/Fruits');
+const Guild = require('../../utils/models/Guild');
 
 module.exports = { 
     config: {
@@ -13,6 +14,8 @@ module.exports = {
       let db = Fruits;
       let option = args[args.length - 1];
       let fruit;
+      let guild = await Guild.findOne({ guildId: message.guild.id })
+      let prefix = guild ? guild.prefix : process.env.PREFIX;
 
       if(message.member.roles.cache.has('720243516734832711')){
         if(args[0] === 'multiple'){
@@ -32,7 +35,7 @@ module.exports = {
             fruit.indexOf("Phoenix") > -1 ? fruit.splice(fruit.indexOf("Phoenix"), 1) : false;
             fruit.push('Bird: Phoenix');
           }
-          if(fruit.length == 1) return message.channel.send(`Use \`stock change ${fruit} ${option}\` instead!`);
+          if(fruit.length == 1) return message.channel.send(`Use ${prefix == 'stock' ? `\`${prefix} change ${fruit} ${option}\`` : `\`${prefix}change ${fruit} ${option}\``} instead!`);
           fruit.map(async f => {
             await db.findOneAndUpdate({ name: f }, { $set: { instock: option } }, (err, doc) => {
               if(doc){ 
@@ -58,7 +61,7 @@ module.exports = {
           } else {
             return message.channel.send('Option Invalid!')
           }
-          if(args.length > 2) return message.channel.send(`Use \`stock change multiple ${args.slice(0, -1).join(' ')} ${option}\` instead!`)
+          if(args.length > 2) return message.channel.send(`Use ${prefix == 'stock' ? `\`${prefix} change multiple ${args.slice(0, -1).join(' ')} ${option}\`` : `\`${prefix}change multiple ${args.slice(0, -1).join(' ')} ${option}\``} instead!`)
           //db update
           await db.findOneAndUpdate({ name: fruit }, { $set: { instock: option } }, (err, doc) => {
             if(doc){ 
